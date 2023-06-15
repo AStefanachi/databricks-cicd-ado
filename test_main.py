176cfda0e5cf6ee2dd3cfe11918c18d824133796
andrea.stefanachi <andrea.stefanachi@macaw.net>
2023-06-15 05:23:24 +0200

    added assertion_fail_on_purpose

--- test_main.py
+++ test_main.py
@@ -11,9 +11,9 @@ class CovidData(NutterFixture):
         first_row = tbl.first()
         assert (first_row[0] >= 1)
 
-    # def assertion_your_assertion(self):
-    #     # your code
-    #     assert (your_assertion_condition)
+# Uncomment the following lines if you want to experience what happens if a test fails
+    # def assertion_fail_on_purpose(self):
+    #     assert (1 == 2)
 # COMMAND ----------
 # DBTITLE 1, Execute the tests and print the results
 result = CovidData().execute_tests()
